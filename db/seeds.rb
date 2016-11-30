2df2e5952cce0d5495d20ae45fc4abccaa03ab80
Daniel Bidulock <daniel@capitolhill.ca>
2016-11-30 19:02:22 +0000

    Correct seed data setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,19 +15,19 @@ caches = [
     description: "Abandoned Press Board",
     coordinates: "51.1480255,-114.2352218611111",
     image: "/images/press_board.jpg",
-    user_id: danny
+    user: danny
   },
   {
     description: "Metal shelving and pipes",
     coordinates: "51.154060361111114,-114.16134641666667",
     image: "/images/metal_shelving.jpg",
-    user_id: danny
+    user: danny
   },
   {
     description: "Cabinets and scrap wood",
     coordinates: "51.13459777777778,-114.15988158333333",
     image: "/images/cabinets_and_scrap_wood.jpg",
-    user_id: lanny
+    user: lanny
   }    
 ]
 
